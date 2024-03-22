2760ab909e0fec445eacc797610c8696e5670370
sydleither <sydleither@protonmail.com>
2024-03-22 10:17:33 -0400

    add initial experiments and analysis

---
+++ agg_experiment.py
@@ -0,0 +1,78 @@
+import os
+import sys
+import warnings
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+sns.set_palette(sns.color_palette("pastel"))
+warnings.filterwarnings("ignore")
+
+
+def print_info(df, measure):
+    df = df.drop(columns=["optimized"])
+    best = {x:0 for x in df["exp_name"].unique()}
+    for property in df["property"].unique():
+        dfp = df.loc[df["property"] == property]
+        for objectives in dfp["objectives"].unique():
+            dfpo = dfp.loc[dfp["objectives"] == objectives]
+            dfpo = dfpo.drop(columns=["property", "objectives", "rep"])
+            dfpo = dfpo.groupby(["exp_name"]).mean()
+            best_row = dfpo[dfpo[measure] == dfpo[measure].max()]
+            best[best_row.index[0]] += 1
+    print(f"{measure} {best}")
+
+
+def plot_diversity(df, exp_name, measure):
+    figure, axis = plt.subplots(4, 4, figsize=(24,24))
+    row = 0
+    col = 0
+    for property in df["property"].unique():
+        if property.endswith("distribution"):
+            continue
+        sns.boxplot(data=df.loc[df["property"] == property], x="objectives", y=measure, hue="exp_name", ax=axis[row][col])
+        axis[row][col].set_title(property)
+        row += 1
+        if row % 4 == 0:
+            col += 1
+            row = 0
+    figure.tight_layout()
+    plt.savefig(f"{exp_name}/{measure}.png")
+    plt.close()
+
+
+def get_data(exp_dir):
+    df = pd.DataFrame(columns=["exp_name", "objectives", "rep", "property", "entropy", "uniformity", "spread", "optimized"])
+    for experiment in os.listdir(exp_dir):
+        if os.path.isfile(f"{exp_dir}/{experiment}"):
+            continue
+        for param_combo in os.listdir(f"{exp_dir}/{experiment}"):
+            for replicate in os.listdir(f"{exp_dir}/{experiment}/{param_combo}"):
+                full_path = f"{exp_dir}/{experiment}/{param_combo}/{replicate}"
+                if os.path.isfile(full_path):
+                    continue
+                df_i = pd.read_csv(f"{full_path}/diversity.csv")
+                df_i["exp_name"] = experiment
+                df_i["objectives"] = param_combo
+                df_i["rep"] = replicate
+                fitnesses = pd.read_pickle(f"{full_path}/fitness_log.pkl")
+                fitnesses = {k:v[-1] for k,v in fitnesses.items()}
+                for objective in fitnesses.keys():
+                    df_i.loc[df_i["property"] == objective, "optimized"] = "yes" if fitnesses[objective] == 0 else "no"
+                df = pd.concat([df, df_i])
+    return df
+
+
+def main(exp_name):
+    df = get_data(exp_name)
+    plot_diversity(df, exp_name, "uniformity")
+    plot_diversity(df, exp_name, "spread")
+    print_info(df, "spread")
+    print_info(df, "uniformity")
+    print_info(df, "entropy")
+
+
+if __name__ == "__main__":
+    experiment_name = sys.argv[1]
+    main(experiment_name)
---
+++ make_configs.py
@@ -0,0 +1,99 @@
+import json
+import os
+import sys
+
+
+def experiment_config(save_dir, exp_dir, exp_name, eval_funcs, network_size=10, num_generations=1000, popsize=500, mutation_rate=0.005):
+    config = {
+        "data_dir": exp_dir,
+        "name": exp_name,
+        "reps": 1,
+        "save_data": 1,
+        "plot_data": 0,
+        "mutation_rate": mutation_rate,
+        "mutation_odds": [1,2,1,2],
+        "crossover_odds": [1,2,2],
+        "crossover_rate": 0.6,
+        "weight_range": [-1,1],
+        "popsize": popsize,
+        "network_size": network_size,
+        "num_generations": num_generations,
+        "eval_funcs": eval_funcs
+    }
+
+    config_path = f"{save_dir}{exp_dir}/{exp_name}.json"
+    with open(config_path, "w") as f:
+        json.dump(config, f, indent=4)
+
+
+def parameter_tuning(exp_dir, network_size, num_generations, popsize, mutation_rate):
+    configs_path = "configs/"
+    if not os.path.exists(configs_path+exp_dir):
+        os.makedirs(configs_path+exp_dir)
+    if not os.path.exists(exp_dir):
+        os.makedirs(exp_dir)
+
+    eval_funcs = {}
+    eval_funcs["c25"] = {"connectance": 0.25}
+    eval_funcs["c75"] = {"connectance": 0.75}
+    eval_funcs["pip"] = {"positive_interactions_proportion": 0.25}
+    eval_funcs["pipapis"] = {"positive_interactions_proportion": 0.25, "average_positive_interactions_strength": 0.25}
+    eval_funcs["cpip"] = {"connectance": 0.2, "positive_interactions_proportion": 0.25}
+    eval_funcs["dd"] = {"in_degree_distribution": [0.0, 0.5, 0.3, 0.1, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]}
+
+    config_names = []
+    for exp_name,eval_func in eval_funcs.items():
+        experiment_config(configs_path, exp_dir, exp_name, eval_func, network_size, num_generations, popsize, mutation_rate)
+        config_names.append(exp_name)
+        os.makedirs(exp_dir+"/"+exp_name)
+
+    submit_output, analysis_output = generate_scripts_batch(exp_dir, config_names)
+    return submit_output, analysis_output
+
+
+def generate_scripts_batch(exp_name, config_names):
+    code_location = os.getcwd()+"/"
+    configs_path = "configs/"
+
+    submit_output = []
+    analysis_output = []
+    for config_name in config_names:
+        submit_output.append(f"sbatch {code_location}run_config.sb {configs_path}{exp_name}/{config_name}.json {exp_name}\n")
+        analysis_output.append(f"python3 graph-evolution/replicate_analysis.py {exp_name}/{config_name}\n")
+
+    return submit_output, analysis_output
+
+
+def write_scripts_batch(exp_name, submit_output, analysis_output):
+    configs_path = "configs/"
+
+    with open(f"{configs_path}submit_{exp_name}_experiments", "w") as f:
+        for output_line in submit_output:
+            f.write(output_line)
+
+    with open(f"configs/analyze_{exp_name}_experiments", "w") as f:
+        for output_line in analysis_output:
+            f.write(output_line)
+
+
+if __name__ == "__main__":
+    experiment_name = sys.argv[1]
+    submit_output = []
+    analysis_output = []
+    if experiment_name == "diversity":
+        for numgen in [500, 1000]:
+            for popsize in [250, 500]:
+                s, a = parameter_tuning(experiment_name+f"_{numgen}gen_{popsize}pop", 10, numgen, popsize, 0.005)
+                submit_output += s
+                analysis_output += a
+    elif experiment_name == "mutation":
+        mutation_rates = [0.005, 0.01, 0.05]
+        file_names = [1, 2, 10]
+        for i in range(len(mutation_rates)):
+            s, a = parameter_tuning(experiment_name+f"_{file_names[i]}", 10, 1000, 500, mutation_rates[i])
+            submit_output += s
+            analysis_output += a
+    else:
+        print("Invalid experiment name.")
+        exit()
+    write_scripts_batch(experiment_name, submit_output, analysis_output)
