cb1d9a0e157699d066561798fd614ed37a2f3dd9
sydleither <sydleither@protonmail.com>
2024-08-19 16:37:59 -0400

    update analysis script to have more details

--- agg_experiment.py
+++ agg_experiment.py
@@ -1,10 +1,17 @@
+from collections import Counter
 import os
+import pickle
+from random import sample
 import sys
 import warnings
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
+
+from eval_functions import functions
+from plot_utils import get_perfect_pop
 
 sns.set_palette(sns.color_palette("pastel"))
 warnings.filterwarnings("ignore")
@@ -23,6 +30,49 @@ def print_info(df, measure):
             best_row = dfpo[dfpo[measure] == dfpo[measure].max()]
             best[best_row.index[0]] += 1
     print(f"{measure} {best}")
+
+
+def print_info_topology(df, measure):
+    best_top = {x:0 for x in df["exp_name"].unique()}
+    best_ew = {x:0 for x in df["exp_name"].unique()}
+    for property in ["connectance", "clustering_coefficient", "diameter", "transitivity", "proportion_of_self_loops"]:
+        dfp = df.loc[(df["property"] == property) & (df["optimized"].isnull())]
+        for objectives in dfp["objectives"].unique():
+            if property == "connectance" and "dd" in objectives:
+                continue
+            dfpo = dfp.loc[dfp["objectives"] == objectives]
+            dfpo = dfpo.drop(columns=["property", "objectives", "rep", "optimized"])
+            dfpo = dfpo.groupby(["exp_name"]).mean()
+            best_row = dfpo[dfpo[measure] == dfpo[measure].max()]
+            if objectives in ["pip", "pipapis", "cpip"]:
+                best_ew[best_row.index[0]] += 1
+            if objectives in ["dd", "c", "ccc", "cpip"]:
+                best_top[best_row.index[0]] += 1
+    print("topological diversity on topological objectives")
+    print(f"{measure} {best_top}")
+    print("topological diversity on edge-weight objectives")
+    print(f"{measure} {best_ew}")
+
+
+def print_info_edgeweights(df, measure):
+    best_top = {x:0 for x in df["exp_name"].unique()}
+    best_ew = {x:0 for x in df["exp_name"].unique()}
+    for property in ["average_positive_interactions_strength", "average_negative_interactions_strength", 
+                     "positive_interactions_proportion", "proportion_of_self_loops_positive"]:
+        dfp = df.loc[(df["property"] == property) & (df["optimized"].isnull())]
+        for objectives in dfp["objectives"].unique():
+            dfpo = dfp.loc[dfp["objectives"] == objectives]
+            dfpo = dfpo.drop(columns=["property", "objectives", "rep", "optimized"])
+            dfpo = dfpo.groupby(["exp_name"]).mean()
+            best_row = dfpo[dfpo[measure] == dfpo[measure].max()]
+            if objectives in ["pip", "pipapis", "cpip"]:
+                best_ew[best_row.index[0]] += 1
+            if objectives in ["dd", "c", "ccc", "cpip"]:
+                best_top[best_row.index[0]] += 1
+    print("edge-weight diversity on topological objectives")
+    print(f"{measure} {best_top}")
+    print("edge-weight diversity on edge-weight objectives")
+    print(f"{measure} {best_ew}")
 
 
 def plot_diversity(df, exp_name, measure):
@@ -49,18 +99,39 @@ def get_data(exp_dir):
     for experiment in os.listdir(exp_dir):
         if os.path.isfile(f"{exp_dir}/{experiment}"):
             continue
+        print(experiment)
         for param_combo in os.listdir(f"{exp_dir}/{experiment}"):
             for replicate in os.listdir(f"{exp_dir}/{experiment}/{param_combo}"):
                 full_path = f"{exp_dir}/{experiment}/{param_combo}/{replicate}"
                 if os.path.isfile(full_path):
                     continue
+                fitnesses = pd.read_pickle(f"{full_path}/fitness_log.pkl")
+                fitnesses = {k:v[-1] for k,v in fitnesses.items()}
                 df_i = pd.read_csv(f"{full_path}/diversity.csv")
+                N = 500
+                if "nsga" not in experiment:
+                    if df_i["optimized_size"].values[0] > N:
+                        with open(f"{full_path}/final_pop.pkl", "rb") as f:
+                            population = pickle.load(f)
+                            perfect_pop = get_perfect_pop(population, fitnesses)
+                            perfect_pop_sample = sample(perfect_pop, N)
+                            rows = []
+                            for name in functions:
+                                typeCounter = Counter([organism.getProperty(name) 
+                                                    if "distribution" not in name 
+                                                    else tuple(organism.getProperty(name)) 
+                                                    for organism in perfect_pop_sample])
+                                entropy = -sum([(count/N)*np.log2(count/N) for count in typeCounter.values()])
+                                uniformity = entropy / np.log2(len(typeCounter))
+                                spread = len(typeCounter)
+                                final_pop_size = len(population)
+                                optimized_size = N
+                                rows.append([name, entropy, uniformity, spread, final_pop_size, optimized_size])
+                            df_i = pd.DataFrame(rows, columns=["property","entropy","uniformity","spread","final_pop_size","optimized_size"])
                 df_i["exp_name"] = experiment
                 df_i["objectives"] = param_combo
                 df_i["rep"] = replicate
-                fitnesses = pd.read_pickle(f"{full_path}/fitness_log.pkl")
-                fitnesses = {k:v[-1] for k,v in fitnesses.items()}
-                for objective in fitnesses.keys():
+                for objective in fitnesses:
                     df_i.loc[df_i["property"] == objective, "optimized"] = "yes" if fitnesses[objective] == 0 else "no"
                 df = pd.concat([df, df_i])
     return df.reset_index()
@@ -68,12 +139,17 @@ def get_data(exp_dir):
 
 def main(exp_name):
     df = get_data(exp_name)
+
     plot_diversity(df, exp_name, "uniformity")
     plot_diversity(df, exp_name, "spread")
     plot_diversity(df, exp_name, "entropy")
+    print()
     print_info(df, "spread")
     print_info(df, "uniformity")
     print_info(df, "entropy")
+    print()
+    print_info_edgeweights(df, "spread")
+    print_info_topology(df, "spread")
 
 
 if __name__ == "__main__":
